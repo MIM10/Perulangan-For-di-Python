56a9c5924796c30922d215aef162d8d64a1d4b48
Abdullah Qaid Mu'aadz <107825990+MIM10@users.noreply.github.com>
2022-07-14 11:12:27 +0700

    Add files via upload

---
+++ Perulangan For di Python.py
@@ -0,0 +1,51 @@
+# membuat array siswa
+siswa = []
+
+# membuat perulangan array
+for i in range(3):
+
+    # mengecek nilai indeks
+    print("")
+    print("ini adalah indeks ke-", i)
+
+    # menerima inputan nama
+    nama_siswa = input("Masukan nama Siswa : ")
+
+    # menerima inputan daerah
+    nama_daerah = input("Masukan nama daerah : ")
+
+    # memasukan hasil inputan nama ke array siswa
+    siswa.append(nama_siswa)
+
+print("")
+
+# membuat perulangan untuk mencetak data dari array siswa
+for k in siswa:
+
+    # mencetak array siswa
+    print("Nama Siswa : ", k)# membuat array siswa
+siswa = []
+
+# membuat perulangan array
+for i in range(3):
+
+    # mengecek nilai indeks
+    print("")
+    print("ini adalah indeks ke-", i)
+
+    # menerima inputan nama
+    nama_siswa = input("Masukan nama Siswa : ")
+
+    # menerima inputan daerah
+    nama_daerah = input("Masukan nama daerah : ")
+
+    # memasukan hasil inputan nama ke array siswa
+    siswa.append(nama_siswa)
+
+print("")
+
+# membuat perulangan untuk mencetak data dari array siswa
+for k in siswa:
+
+    # mencetak array siswa
+    print("Nama Siswa : ", k)
